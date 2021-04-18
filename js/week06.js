ab8cd11901ef402f3f0393c55d2f166ff020ea16
Guteberg Cruz <gute.tec@gmail.com>
2021-04-17 22:16:21 -0300

    week06

---
+++ js/week06.js
@@ -0,0 +1,11 @@
+$(document).ready(function () {
+  $('.paragrafos p:nth-of-type(1)').hover(function () {
+    $('.c-6').css('transform', 'rotateZ(1deg)')
+  });
+  $('.paragrafos p:nth-of-type(2)').hover(function () {
+    $('.c-6').css('transform', 'rotateZ(91deg)')
+  });
+  $('.paragrafos p:nth-of-type(3)').hover(function () {
+    $('.c-6').css('transform', 'rotateZ(181deg)')
+  });
+});
